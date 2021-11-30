5f0f6ce92f29f0450a4cca989b849c1bdaae47fe
lindacheung <lindacheung94@gmail.com>
2021-11-30 15:20:47 +0800

    view-cat

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :pets do
     collection do
-      get :dog, :cat
+      get :dog
+      get :cat
     end
   end
 end
