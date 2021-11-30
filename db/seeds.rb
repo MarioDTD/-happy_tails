6997b7d172c1f710f29827218c8f6ef12f607db5
Mario Stefan Juhari <msjdtd@gmail.com>
2021-11-30 12:37:41 +0800

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 require 'faker'
 
 Puts 'Destroying data'
@@ -48,14 +47,7 @@ end
 end
 
 puts 'Pets created'
-=======
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
 puts "clearing users..."
 User.destroy_all
 
@@ -73,4 +65,3 @@ User.create!(
   admin: true
 )
 puts "Admin user created"
->>>>>>> 50527c269e3cc5dc19f24e6d12a06a572c3057f8
