797bd1b0fcf6fa2c1a4533903d3c4b0b4047cfd4
Mario Stefan Juhari <msjdtd@gmail.com>
2021-11-30 12:55:09 +0800

    seeds works

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,54 +1,9 @@
 require 'faker'
 
-Puts 'Destroying data'
+puts 'Destroying data'
 
 Shelter.destroy_all
 Pet.destroy_all
-
-puts 'Creating shelters'
-
-happy_pets = Shelter.create!(
-  name: "SPCA",
-  address: Faker::Address.street_address,
-  contact: Faker::PhoneNumber.cell_phone,
-  email:  Faker::Internet.email
-)
-
-puts "Shelter created"
-
-puts 'Creating pets'
-
-5.times do
-  dog = Pet.new(
-    name: Faker::Name.first_name,
-    animal: "Dog",
-    breed: Faker::Creature::Dog.breed,
-    color: Faker::Color.color_name
-    fee: rand(100..500)
-    age: rand(1..15)
-    sterilised: ["Yes", "No"].sample
-    image_url: "https://placedog.net/640/480?random"
-    adopted: [true, false].sample
-  )
-end
-
-5.times do
-  cat = Pet.new(
-    name: Faker::Name.first_name,
-    animal: "Dog",
-    breed: Faker::Creature::Cat.breed,
-    color: Faker::Color.color_name
-    fee: rand(100..500)
-    age: rand(1..15)
-    sterilised: ["Yes", "No"].sample
-    image_url: "https://placekitten.com/g/200/300"
-    adopted: [true, false].sample
-  )
-end
-
-puts 'Pets created'
-
-puts "clearing users..."
 User.destroy_all
 
 puts "Creating normal user"
@@ -59,9 +14,56 @@ User.create!(
 puts "Normal user created!"
 
 puts "Creating admin user"
-User.create!(
+admin_user = User.create!(
   email: 'paul@gmail.com',
   password: '123123',
   admin: true
 )
 puts "Admin user created"
+
+
+puts 'Creating shelters'
+
+happy_pets = Shelter.create!(
+  name: "SPCA",
+  address: Faker::Address.street_address,
+  contact: Faker::PhoneNumber.cell_phone,
+  email:  Faker::Internet.email,
+  user: admin_user
+)
+
+puts "Shelter created"
+
+puts 'Creating pets'
+
+5.times do
+  Pet.create!(
+    name: Faker::Name.first_name,
+    animal: "Dog",
+    breed: Faker::Creature::Dog.breed,
+    color: Faker::Color.color_name,
+    fee: rand(100..500),
+    age: rand(1..15),
+    sterilised: ["Yes", "No"].sample,
+    image_url: "https://placedog.net/640/480?random",
+    adopted: [true, false].sample,
+    shelter: happy_pets
+  )
+end
+
+5.times do
+  Pet.create!(
+    name: Faker::Name.first_name,
+    animal: "Dog",
+    breed: Faker::Creature::Cat.breed,
+    color: Faker::Color.color_name,
+    fee: rand(100..500),
+    age: rand(1..15),
+    sterilised: ["Yes", "No"].sample,
+    image_url: "https://placekitten.com/g/200/300",
+    adopted: [true, false].sample,
+    shelter: happy_pets
+  )
+end
+
+puts 'Pets created'
