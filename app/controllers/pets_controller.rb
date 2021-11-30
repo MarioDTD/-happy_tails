acb3ad5c3c5ffe1e3e798cb0bef5e9fadd6ede6b
iman fuad <nooreliman@gmail.com>
2021-11-30 15:36:01 +0800

    added form to edit pet params

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -10,4 +10,15 @@ class PetsController < ApplicationController
   def dog
     @dogs = Pet.where(animal: "Dog")
   end
+
+  def update
+    @pet = Pet.find(params[:id])
+    @pet.update(pet_params)
+    redirect_to pet_path(@pet)
+  end
+
+  private
+  def pet_params
+    params.require(:pet).permit(:name, :breed, :color, :fee, :age, :sterilised)
+  end
 end
