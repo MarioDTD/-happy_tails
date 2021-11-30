3235d4af38e3851206e2e23fa36fb081491c1d1e
Mario Stefan Juhari <msjdtd@gmail.com>
2021-11-30 12:31:28 +0800

    creating seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,49 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Puts 'Destroying data'
+
+Shelter.destroy_all
+Pet.destroy_all
+
+puts 'Creating shelters'
+
+happy_pets = Shelter.create!(
+  name: "SPCA",
+  address: Faker::Address.street_address,
+  contact: Faker::PhoneNumber.cell_phone,
+  email:  Faker::Internet.email
+)
+
+puts "Shelter created"
+
+puts 'Creating pets'
+
+5.times do
+  dog = Pet.new(
+    name: Faker::Name.first_name,
+    animal: "Dog",
+    breed: Faker::Creature::Dog.breed,
+    color: Faker::Color.color_name
+    fee: rand(100..500)
+    age: rand(1..15)
+    sterilised: ["Yes", "No"].sample
+    image_url: "https://placedog.net/640/480?random"
+    adopted: [true, false].sample
+  )
+end
+
+5.times do
+  cat = Pet.new(
+    name: Faker::Name.first_name,
+    animal: "Dog",
+    breed: Faker::Creature::Cat.breed,
+    color: Faker::Color.color_name
+    fee: rand(100..500)
+    age: rand(1..15)
+    sterilised: ["Yes", "No"].sample
+    image_url: "https://placekitten.com/g/200/300"
+    adopted: [true, false].sample
+  )
+end
+
+puts 'Pets created'
