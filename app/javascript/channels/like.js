489d7b4c85f167f01cdb4cf53d05d1098f8e2a3c
Rachel Toh <Racheltohxy@gmail.com>
2021-12-01 13:13:26 +0800

    Delete like.js

--- app/javascript/channels/like.js
+++
@@ -1,9 +0,0 @@
-const likeInput = function () {
-  const likeButton = document.getElementById("like_button");
-  likeButton.addEventListener("click", (event) => {
-    const likeButton = document.getElementById("like_button");
-    likeButton.value = likeButton.value === "❤️" ? "♡" : "❤️";
-  });
-};
-
-export { likeInput };
