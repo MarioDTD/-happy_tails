612a0f668f1313b90d5866d9c522abdea508821e
rachel toh <racheltohxy@gmail.com>
2021-12-03 14:51:56 +0800

    implemented share js

--- app/javascript/packs/share.js
+++ app/javascript/packs/share.js
@@ -1,5 +1,15 @@
 const initShare = () => {
-  console.log("calling the function!");
+  const share = document.querySelector(".share");
+  const sharedSpan = document.querySelector(".shared-span");
+  share.addEventListener("click", (event) => {
+    const url = window.location.href;
+    navigator.clipboard
+      .writeText(url)
+      .then(() => {
+        sharedSpan.classList.remove("d-none");
+      })
+      .catch(() => console.log("something went wrong"));
+  });
 };
 
 export { initShare };
